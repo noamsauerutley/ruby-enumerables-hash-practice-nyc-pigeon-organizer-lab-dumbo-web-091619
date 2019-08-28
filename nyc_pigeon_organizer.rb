d510d115a4d034506e8d4c7d955ef37990136938
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-28 16:55:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,50 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organized_pigeons = {}
+
+    data.each do |key, hash|
+      hash.each do |category, info|
+        info.each do |name|
+        organized_pigeons[name] = {
+          :color =>[],
+          :gender =>[],
+          :lives =>[]
+        }
+        end
+    end
+  end
+
+pigeons = organized_pigeons.keys
+
+data[:color].each do |pigeon_color, name|
+  name.each do |pigeon_name|
+    pigeons.each do |pigeon|
+      if pigeon_name == pigeon
+        organized_pigeons[pigeon][:color] << pigeon_color.to_s
+      end
+    end
+  end
+end
+
+data[:gender].each do |pigeon_gender, name|
+  name.each do |pigeon_name|
+    pigeons.each do |pigeon|
+      if pigeon_name == pigeon
+        organized_pigeons[pigeon][:gender] << pigeon_gender.to_s
+      end
+    end
+  end
+end
+
+data[:lives].each do |pigeon_lives, name|
+  name.each do |pigeon_name|
+    pigeons.each do |pigeon|
+      if pigeon_name == pigeon
+        organized_pigeons[pigeon][:lives] << pigeon_lives.to_s
+      end
+    end
+  end
+end
+
+  return organized_pigeons
 end
