16f51ccbb0757ffb2b812328bc806efc50de9598
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-28 16:34:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,8 @@ def nyc_pigeon_organizer(data)
 
   organized_pigeons.each_key do |pigeon|
     data.each_key do |key, hash|
-      if data[hash].includes?(pigeon)
+      hash.each do |element|
+      if element == (pigeon)
         organized_pigeons[pigeon][key] = hash
       end
     end
