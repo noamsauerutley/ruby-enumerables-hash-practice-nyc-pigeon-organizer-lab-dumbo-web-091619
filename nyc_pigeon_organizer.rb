be49e22a8be1c66a255d3b162e18029cbea8f63a
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-28 16:41:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,15 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  organized_pigeons = {
-    "Theo" => {},
-    "Peter Jr." => {},
-    "Lucky" => {},
-    "Ms. K" => {},
-    "Queenie" => {},
-    "Andrew" => {},
-    "Alex" => {}
-  }
+  organized_pigeons = {}
 
     data.each_key do |key, hash|
       hash.each do |category, info|
-        if data[hash] == pigeon
-          organized_pigeons[pigeon][key] = hash
+        info.each do |name|
+        organized_pigeons[name] = {
+          :color =>[],
+          :gender =>[],
+          :lives =>[]
+        }
         end
     end
   end
