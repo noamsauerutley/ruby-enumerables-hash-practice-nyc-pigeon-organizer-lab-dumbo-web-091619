193fc3052310b44da3d440cb575df291da55a8cd
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-28 16:51:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,12 +15,22 @@ def nyc_pigeon_organizer(data)
   end
 
 pigeons = organized_pigeons.keys
+
 data[:color].each do |pigeon_color, name|
   name.each do |pigeon_name|
     pigeons.each do |pigeon|
       if pigeon_name == pigeon
         organized_pigeons[pigeon][:color] << pigeon_color.to_s
       end
+    end
+  end
+end
+
+data[:gender].each do |pigeon_gender, name|
+  name.each do |pigeon_name|
+    pigeons.each do |pigeon|
+      if pigeon_name == pigeon
+        organized_pigeons[pigeon][:gender]<< pigeon_color.to_s
     end
   end
 end
