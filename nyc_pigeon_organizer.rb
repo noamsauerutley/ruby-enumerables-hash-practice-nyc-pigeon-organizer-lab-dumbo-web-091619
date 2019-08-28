dbe0d00b7efc94ce5dd8eb6c0ffa693658887c4e
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-28 16:14:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  return data
+  organized_pigeons = {
+    "Theo" => nil,
+    "Peter Jr." => nil, 
+    "Lucky" => nil, 
+    "Mrs. K" => nil, 
+    "Queenie" => nil, 
+    "Andrew" => nil, 
+    "Alex" => nil }
+
+  data[0].each do |trait, data|
+    pigeons.each do |pigeon|
+      if data[0][trait][data].includes?(pigeon)
+    end
+  end
+  organized_pigeons
 end
